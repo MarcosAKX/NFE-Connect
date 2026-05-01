38dd1a15845bf6298a17228c0e20d53677283cb1
MarcosAKX <marcosth3060@gmail.com>
2026-05-01 19:35:11 -0300

    Fazer Login

--- navegador.py
+++ navegador.py
@@ -1,16 +1,33 @@
 from playwright.sync_api import sync_playwright
 import pandas as pd
+import os
+from dotenv import load_dotenv
 
 
-def iniciar_navegador(url):
-    #Iniciar o playwright
-    playwright = sync_playwright().start()
-    #Instanciar e abrir o navegador.
-    #Headless - Define se eu vejo o navegador ou não.
-    browser = playwright.chromium.launch(headless=False)
-    #Cria um perfil novo no navegador
- 
+load_dotenv()
 
-url = "https://nfe.etransparencia.com.br/sp.bebedouro/nfe/principal.aspx?6Mq+8H81l6bBzU51Aa4BxIysvNcaBQlI0zjF2rJpzQc="
+class EmissorNFE:
+    #Preparar o ambiente web e ligar o motor playwright.
+    def __init__(self):
+        self.playwright = sync_playwright().start()
+        self.browser = self.playwright.chromium.launch(headless=False)
+        self.page = self.browser.new_page()
+        self.page.goto("https://nfe.etransparencia.com.br/sp.bebedouro/nfe/principal.aspx?6Mq+8H81l6bBzU51Aa4BxIysvNcaBQlI0zjF2rJpzQc=")
 
-playwright, browser, page = iniciar_navegador(url)
+    def Login(self,usuario,senha):
+        #Localiza dentro da tag <a> um texto que contém "Login" e clica nele.
+        self.page.locator("a:has-text('Login')").click()
+        self.page.fill('#W0045vUSUARIO',usuario)
+        self.page.fill('#W0045vSENHA', senha)
+        self.page.wait_for_timeout(3000)
+   
+
+    def captcha(self):
+        pass
+
+if __name__ == "__main__":
+    bot = EmissorNFE()
+
+    cpf = os.getenv("CPF")
+    senha = os.getenv("SENHA")
+    bot.Login(usuario=cpf, senha=senha)
